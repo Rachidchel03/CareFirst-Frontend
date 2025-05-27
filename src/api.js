2e8a16fe4c3d4a14f844adecd0bd908e14e5fb76
Rachid003 <an0n.3m@hotmail.com>
2025-05-27 12:56:51 +0200

    changed api.js

--- src/api.js
+++ src/api.js
@@ -1,10 +1,18 @@
-// src/api.js
+import axios from "axios";
 
-import axios from 'axios';
+// pick up your Render (or localhost) backend URL from env
+const API = process.env.REACT_APP_BACKEND_URL || "";
+
+// create a shared client
+const client = axios.create({
+  baseURL: API,
+  // you can add headers, interceptors, etc, here
+});
 
 export async function fetchBestemming(address) {
-  const resp = await axios.get("http://localhost:8000/api/bestemming", {
-    params: { address }
-  });
+  // no more hard-coded localhost: use the client baseURL
+  const resp = await client.get("/api/bestemming", { params: { address } });
   return resp.data;  // { bestemming: [...] }
 }
+
+export default client;
