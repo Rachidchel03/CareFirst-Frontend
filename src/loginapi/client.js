06c6d5dc52db300fe5f038fecfb099cb214cdb73
Rachid003 <an0n.3m@hotmail.com>
2025-06-25 16:13:20 +0200

    changed client.js

--- src/loginapi/client.js
+++ src/loginapi/client.js
@@ -1,14 +1,11 @@
 // src/api/client.js
 import axios from "axios";
-import { useAuth } from "../Authentication/AuthContext";
-import React, { useState } from "react";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL,
-  withCredentials: true,        // ← ensures cookies/auth headers are included
+  baseURL: process.env.REACT_APP_API_BASE_URL,  // e.g. https://carefirst-backend-2.onrender.com
+  withCredentials: true,
   headers: { "Content-Type": "application/json" },
 });
-
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
