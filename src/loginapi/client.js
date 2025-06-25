f92852ab1d3acf05bcf3aad9bcfe20ddd472e723
Rachid003 <an0n.3m@hotmail.com>
2025-06-25 15:47:46 +0200

    changed the ocalhost

--- src/loginapi/client.js
+++ src/loginapi/client.js
@@ -4,7 +4,7 @@ import { useAuth } from "../Authentication/AuthContext";
 import React, { useState } from "react";
 
 const api = axios.create({
-  baseURL:  process.env.REACT_APP_API_BASE_URL || "http://localhost:8000", 
+  baseURL:  process.env.REACT_APP_API_BASE_URL || " ", 
   headers: { "Content-Type": "application/json" },
   withCredentials: true,   // if you need cookies
 });
